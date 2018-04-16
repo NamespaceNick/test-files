e8002ee21a37d628fbc8b4b9ef9ad2a2bd32358a
Nicolas Williams <ndwill@umich.edu>
2018-04-15 21:25:46 -0400

    Saved C file. <assert.h>

--- clean-files/ctest_file.c
+++ clean-files/ctest_file.c
@@ -1,10 +1,11 @@
 // C test file to random inquiries
 //  By Nicolas Williams. January 18th, 2018
 
+#include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
 int main(int argc, char* argv[]){
   return 0;
 }
